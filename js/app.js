51412c0dfc845dcdd2b9e20c744c3a9e15f6ab39
Jesus Polanco <151112353+Jmpd17@users.noreply.github.com>
2026-09-22 15:55:18 -0400

    arreglando algo

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const startButton = document.querySelector("start-button");
+const startButton = document.querySelector("#start-button");
 const statusMessage = document.querySelector("#status-message");
 
 function showStartMessage() {
-    statusMessage.textContent =
-    "¡Todo conectado! El siguiente paso sera contruir el cuestionario";
+  statusMessage.textContent =
+    "¡Todo conectado! El siguiente paso será construir el cuestionario.";
 }
 
 startButton.addEventListener("click", showStartMessage);
